5a7c9f49cc20cdf03c70dbe82195d82a82d3ebfe
iamnotfelix <andrei.iliescu03@gmail.com>
2024-05-14 22:48:06 +0300

    Delete Django server.

--- server/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group
-from .models.attempt import Attempt
-from .models.user import User
-
-admin.site.unregister(Group)
-admin.site.register(User)
-admin.site.register(Attempt)
--- server/api/serializers/authSerializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models.user import User
-
-class AuthSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ['username', 'password']
